690024f3491e3f5de49a385b53a908f287869ec8
Joseph McGreene <josephorianmcgreene@gmail.com>
2021-08-03 14:00:47 -0600

    Create React Build folder, Ready for Deployment

--- src/components/MonsterInfo.js
+++ src/components/MonsterInfo.js
@@ -1,4 +1,15 @@
 function MonsterInfo({ monsterInfo }) {
+  function calculateModifier(skill) {
+    let modifier = Math.floor((skill - 10) / 2);
+
+    if (modifier > 0) {
+      return `+${modifier}`;
+    }
+    if (modifier <= 0) {
+      return modifier;
+    }
+  }
+
   return (
     <section id="monster-info">
       {monsterInfo ? <h2>Name: {monsterInfo.name}</h2> : ""}
@@ -18,12 +29,54 @@ function MonsterInfo({ monsterInfo }) {
         ""
       )}
       <ul className="monster-info-list">
-        {monsterInfo ? <li>Strength: {monsterInfo.strength}</li> : ""}
-        {monsterInfo ? <li>Dexterity: {monsterInfo.dexterity}</li> : ""}
-        {monsterInfo ? <li>Constitution: {monsterInfo.constitution}</li> : ""}
-        {monsterInfo ? <li>Intelligence: {monsterInfo.intelligence}</li> : ""}
-        {monsterInfo ? <li>Wisdom: {monsterInfo.wisdom}</li> : ""}
-        {monsterInfo ? <li>Charisma: {monsterInfo.charisma}</li> : ""}
+        {monsterInfo ? (
+          <li>
+            Strength: {monsterInfo.strength} (
+            {calculateModifier(monsterInfo.strength)})
+          </li>
+        ) : (
+          ""
+        )}
+        {monsterInfo ? (
+          <li>
+            Dexterity: {monsterInfo.dexterity} (
+            {calculateModifier(monsterInfo.dexterity)})
+          </li>
+        ) : (
+          ""
+        )}
+        {monsterInfo ? (
+          <li>
+            Constitution: {monsterInfo.constitution} (
+            {calculateModifier(monsterInfo.constitution)})
+          </li>
+        ) : (
+          ""
+        )}
+        {monsterInfo ? (
+          <li>
+            Intelligence: {monsterInfo.intelligence} (
+            {calculateModifier(monsterInfo.intelligence)})
+          </li>
+        ) : (
+          ""
+        )}
+        {monsterInfo ? (
+          <li>
+            Wisdom: {monsterInfo.wisdom} (
+            {calculateModifier(monsterInfo.wisdom)})
+          </li>
+        ) : (
+          ""
+        )}
+        {monsterInfo ? (
+          <li>
+            Charisma: {monsterInfo.charisma} (
+            {calculateModifier(monsterInfo.charisma)})
+          </li>
+        ) : (
+          ""
+        )}
       </ul>
       {monsterInfo ? <h4>Languages: {monsterInfo.languages}</h4> : ""}
     </section>
