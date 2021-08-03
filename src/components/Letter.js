5c7035580b59961e1e298e73b8988f2a58b04950
Joseph McGreene <josephorianmcgreene@gmail.com>
2021-08-03 14:06:52 -0600

    Updates

--- src/components/Letter.js
+++
@@ -1,5 +0,0 @@
-function Letter({ text, onClick }) {
-  return <li onClick={() => onClick(text.toLowerCase())}>{text}</li>;
-}
-
-export default Letter;
--- src/components/MonsterListItem.js
+++
@@ -1,5 +0,0 @@
-function MonsterListItem({ text, index, onClick }) {
-  return <li onClick={() => onClick(index)}>{text}</li>;
-}
-
-export default MonsterListItem;
